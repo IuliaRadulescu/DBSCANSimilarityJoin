7eb0bda6b61c75d599c05ff1167d2d5c901825df
LillyBlackWhite <iulia@bd-server2.pub.ro>
2019-07-05 20:24:44 +0300

    QuickJoin part

--- src/dbscanMultidimensionalIndex.py
+++ src/dbscanMultidimensionalIndex.py
@@ -1,10 +1,13 @@
-import collections
 import csv
 import sys
+import collections
+from random import randint
 import numpy as np
 import time
-import rtree.index
+#import rtree.index
 from scipy import spatial
+import pymongo
+import mongoConnect
 
 
 class DBSCAN:
@@ -109,23 +112,151 @@ class DBSCANKDtree(DBSCAN):
 			result.append(self.dataset[neighId])
 		return np.array(result)
 
+class quickDBSCAN:
+	def __init__(self, eps):
+		#int
+		self.eps = eps
+		self.mongoConnectInstance = mongoConnect.MongoDBConnector("QuickDBScanDB");
+
+	def randomObject(self, objs):
+		randomIndex = randint(0, len(objs)-1)
+		return objs[randomIndex]
+
+	def euclideanDistPosition(self, a, b):
+		return np.linalg.norm(a-b)
+
+	def ball_average(self, objs, p1):
+		avgDistHelper = []
+		for pixel in objs:
+			if(pixel!=p1):
+				avgDistHelper.append(self.euclideanDistPosition(pixel, p1))
+		avgDistHelper = np.array(avgDistHelper)
+		return sum(avgDistHelper)/len(avgDistHelper)
+
+	def partition(self, objs, p1):
+		partL = [] 
+		partG = []
+		winL = []
+		winG = []
+		
+		r = self.ball_average(objs, p1)
+		startIdx = 0
+		endIdx = len(objs)-1
+		startDist = self.euclideanDistPosition(objs[startIdx], p1)
+		endDist = self.euclideanDistPosition(objs[endIdx], p1)
+		
+		while(startIdx < endIdx):
+			while(endDist > r and startIdx < endIdx):
+				if(endDist <= r+self.eps):
+					winG.append(objs[endIdx])
+				endIdx = endIdx - 1
+				endDist = self.euclideanDistPosition(objs[endIdx], p1)
+				
+			while(startDist <= r and startIdx < endIdx):
+				if(startDist >= r-self.eps):
+					winL.append(objs[startIdx])
+				startIdx = startIdx + 1
+				startDist = self.euclideanDistPosition(objs[startIdx], p1)
+				
+			if(startIdx < endIdx):
+				if(endDist >= r-self.eps):
+					winL.append(objs[endIdx])
+				if(startDist <= r+self.eps):
+					winG.append(objs[startIdx])
+				#exchange items
+				objs[startIdx], objs[endIdx] = objs[endIdx], objs[startIdx]
+				startIdx = startIdx + 1
+				endIdx = endIdx - 1
+				startDist = self.euclideanDistPosition(objs[startIdx], p1)
+				endDist = self.euclideanDistPosition(objs[endIdx], p1)
+		
+		if(startIdx == endIdx):
+			if(endDist > r and endDist <= r+self.eps):
+				winG.append(objs[endIdx])
+			if(startDist <= r and startDist >= r-self.eps):
+				winL.append(objs[startIdx])
+			if(endDist > r):
+				endIdx = endIdx - 1
+				
+		return (objs[0:endIdx], objs[endIdx+1:len(objs)-1], winL, winG)
+
+	def quickJoin(self, objs, constSmallNumber):
+		if(len(objs) < constSmallNumber):
+			self.nestedLoop(self.eps, objs)
+			return;
+			
+		p1 = self.randomObject(objs)
+		
+		(partL, partG, winL, winG) = self.partition(objs, p1)
+		if(len(winL)>0 and len(winG)>0):
+			self.quickJoinWin(self.eps, winL, winG, constSmallNumber)
+		if(len(partG)>0):
+			self.quickJoin(self.eps, partL, constSmallNumber)
+		if(len(partL)>0):
+			self.quickJoin(self.eps, partG, constSmallNumber)
+
+	def quickJoinWin(self, objs1, objs2, constSmallNumber):
+		print("Intra in win")
+		totalLen = len(objs1) + len(objs2)
+		if(totalLen < constSmallNumber):
+			self.nestedLoop2(self.eps, objs1, objs2)
+			return;
+		allObjects = objs1 + objs2
+		p1 = self.randomObject(allObjects)
+
+		(partL1, partG1, winL1, winG1) = self.partition(objs1, p1)
+		(partL2, partG2, winL2, winG2) = self.partition(objs2, p1)
+
+		self.quickJoinWin(winL1, winG2, constSmallNumber)
+		self.quickJoinWin(winG1, winL2, constSmallNumber)
+		self.quickJoinWin(partL1, partL2, constSmallNumber)
+		self.quickJoinWin(partG1, partG2, constSmallNumber)
+
+	def nestedLoop(self, objs):
+		for coord1 in objs:
+			for coord2 in objs:
+				if(coord1 != coord2 and self.euclideanDistPosition(coord1, coord2) <= self.eps):
+					#print(pixel1, pixel2)
+					#insert into Mongo
+					self.upsertPixelValue("quickDBSCAN",{"object":[coord1[0], coord1[1]]}, [coord2[0], coord2[1]])
+					self.upsertPixelValue("quickDBSCAN",{"object":[coord2[0], coord2[1]]}, [coord1[0], coord1[1]])
+
+
+	def nestedLoop2(self, objs1, objs2):
+		for coord1 in objs1:
+			for coord2 in objs2:
+				if(coord1 != coord2 and self.euclideanDistPosition(coord1, coord2) <= self.eps):
+					#print(pixel1, pixel2)
+					#insert into Mongo
+					self.upsertPixelValue("quickDBSCAN",{"object":[coord1[0], coord1[1]]}, [coord2[0], coord2[1]])
+					self.upsertPixelValue("quickDBSCAN",{"object":[coord2[0], coord2[1]]}, [coord1[0], coord1[1]])
+					
+
+
+	def upsertPixelValue(self, collection, filter, epsNeigh):
+		self.mongoConnectInstance.update("quickDBSCAN", filter, {"$push":{"epsNeighs":epsNeigh}})
 			
 if __name__ == '__main__':
+
+	sys.setrecursionlimit(15000)
 
 	#read the csv
 	datasetFilename = sys.argv[1]
 	dataset = list()
+	datasetQuick = list()
 
 	with open(datasetFilename) as csvFile:
 		csvReader = csv.reader(csvFile, delimiter=',')
 		for row in csvReader:
 			dataset.append( (float(row[2]), float(row[3]), 0) )
+			datasetQuick.append( (float(row[2]), float(row[3]), 0) )
 
 	dataset = np.array(dataset)
+	datasetQuick = np.array(datasetQuick)
 
 	print(np.shape(dataset))
 
-	dbscan = DBSCANRtree(1, 5, dataset)
+	'''dbscan = DBSCANRtree(1, 5, dataset)
 
 	start = time.time()
 
@@ -141,7 +272,7 @@ if __name__ == '__main__':
 
 	end = time.time()
 
-	print('DBSCANRtree took '+str(end - start))
+	print('DBSCANRtree took '+str(end - start))'''
 
 	dbscan = DBSCAN(1, 5, dataset)
 
@@ -170,6 +301,9 @@ if __name__ == '__main__':
 	end = time.time()
 
 	print('DBSCANKdtree took '+str(end - start))
+
+	quickDBSCAN = quickDBSCAN(1)
+	quickDBSCAN.quickJoin(datasetQuick, 10)
 
 
 
